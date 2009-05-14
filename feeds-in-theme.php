059f354e6a73acf0219980ccf52874c99ad80865
elebail <elebail@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-14 15:11:33 +0000

    plugin creatiopn
    git-svn-id: https://plugins.svn.wordpress.org/feeds-in-theme/trunk@117918 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feeds-in-theme.php
@@ -0,0 +1,156 @@
+<?php
+/*
+ * Plugin Name: feeds-in-theme
+ * Plugin URI: #
+ * Description: Recreates the standard WordPress feeds to alow using theme templates in the feeds.
+ * Version: 0.0.1
+ * Author: Eric Le Bail
+ * Author URI: #
+ *
+ * This plugin has been developped and tested with Wordpress Version 2.6
+ *
+ * Copyright 2009  Eric Le Bail (email : eric_lebail@hotmail.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */
+
+add_action('init', 'fit_add_feed');
+
+
+/**
+ * Function that creates the custom feeds and add a rewriterules to
+ * call the feeds.
+ * @return void.
+ */
+function fit_add_feed() {
+    global $wp_rewrite;
+    add_feed('trdf', 'fit_do_feed_rdf');
+    add_feed('trss', 'fit_do_feed_rss');
+    add_feed('trss2', 'fit_do_feed_rss2');
+    add_feed('tatom', 'fit_do_feed_atom');
+    add_action('generate_rewrite_rules', 'fit_rewrite_rules');
+    $wp_rewrite->flush_rules();
+}
+
+/**
+ * Function that creates the rewrite rules for custom feeds.
+ * @param $wp_rewrite
+ * @return void.
+ */
+function fit_rewrite_rules($wp_rewrite) {
+    $new_rules = array(
+    'feed/(.+)' => 'index.php?feed='.$wp_rewrite->preg_index(1)
+    );
+    $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+}
+
+
+/**
+ * Load the RDF RSS 0.91 Feed template.
+ *
+ * First check if a template is available in theme then uses the
+ * default wordpress template.
+ */
+function fit_do_feed_rdf() {
+    if ( file_exists(TEMPLATEPATH . '/feed-rdf.php') )
+    {
+        load_template( TEMPLATEPATH . '/feed-rdf.php' );
+    }
+    else
+    {
+        load_template( ABSPATH . WPINC . '/feed-rdf.php' );
+    }
+
+}
+
+/**
+ * Load the RSS 1.0 Feed Template
+ *
+ * First check if a template is available in theme then uses the
+ * default wordpress template.
+ */
+function fit_do_feed_rss() {
+    if ( file_exists(TEMPLATEPATH . '/feed-rss.php') )
+    {
+        load_template( TEMPLATEPATH . '/feed-rss.php' );
+    }
+    else
+    {
+        load_template( ABSPATH . WPINC . '/feed-rss.php' );
+    }
+}
+
+/**
+ * Load either the RSS2 comment feed or the RSS2 posts feed.
+ *
+ * First check if a template is available in theme then uses the
+ * default wordpress template.
+ *
+ * @param bool $for_comments True for the comment feed, false for normal feed.
+ */
+function fit_do_feed_rss2( $for_comments ) {
+    if ( $for_comments )
+    if ( file_exists(TEMPLATEPATH . '/feed-rss2-comments.php') )
+    {
+        load_template( TEMPLATEPATH . '/feed-rss2-comments.php' );
+    }
+    else
+    {
+        load_template( ABSPATH . WPINC . '/feed-rss2-comments.php' );
+    }
+    else
+    if ( file_exists(TEMPLATEPATH . '/feed-rss2.php') )
+    {
+        load_template( TEMPLATEPATH . '/feed-rss2.php' );
+    }
+    else
+    {
+        load_template( ABSPATH . WPINC . '/feed-rss2.php' );
+    }
+}
+
+/**
+ * Load either Atom comment feed or Atom posts feed.
+ *
+ * First check if a template is available in theme then uses the
+ * default wordpress template.
+ *
+ * @param bool $for_comments True for the comment feed, false for normal feed.
+ */
+function fit_do_feed_atom( $for_comments ) {
+    if ($for_comments)
+    {
+        if ( file_exists(TEMPLATEPATH . '/feed-atom-comments.php') )
+        {
+            load_template( TEMPLATEPATH . '/feed-atom-comments.php' );
+        }
+        else
+        {
+            load_template( ABSPATH . WPINC . '/feed-atom-comments.php' );
+        }
+    } else {
+        if ( file_exists(TEMPLATEPATH . '/feed-atom.php') )
+        {
+            load_template( TEMPLATEPATH . '/feed-atom.php' );
+        }
+        else
+        {
+            load_template( ABSPATH . WPINC . '/feed-atom.php' );
+        }
+    }
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Category Meta plugin ===
+Contributors: Eric Le Bail
+Donate link: #
+Tags: custom,custom feed,feed,feeds,rdf,rss,rss2,atom,plugin
+Requires at least: 2.6
+Tested up to: 2.6.5
+Stable tag: trunk
+
+Creates 4 feeds : 'trdf' 'trss', 'trss2' and 'tatom' highly customisable using theme template.
+
+== Description ==
+
+Creates 4 feeds : 
+    trdf => rdf in theme (template : feed-rdf.php)
+    trss => rss 1.0 in theme (template : feed-rss.php)
+    trss2 => rss 2.0 in theme (template : feed-rss2-comments.php and feed-rss2.php)
+    tatom => atom in theme (template : feed-atom-comments.php and feed-atom.php)
+
+Those feeds look for a feed template into the theme directory.
+If no template is found into the theme directory the feed uses the default wordpress feed template.
+
+== Installation ==
+
+1. Unzip into your `/wp-content/plugins/` directory. If you're uploading it make sure to upload
+the top-level folder. Don't just upload all the php files and put them in `/wp-content/plugins/`.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+2. That's it!
+3. you can use create your own feed template into your theme directory.
+
+== Frequently Asked Questions ==
+
+I use this plugin into my own blog sucessfuly.
+If you have any problem / corrections let me now 
+(do not forget to give me the WordPress version You have).
+
